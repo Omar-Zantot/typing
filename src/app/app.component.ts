43671b5ba3ddf8f591b53d68f78c7bf86ed9ef50
Omar-Zantout <omar82199@gmail.com>
2023-09-10 22:34:00 +0300

    typing challenge done 😊

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { loremIpsum } from 'lorem-ipsum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = loremIpsum({ count: 7, format: 'plain', units: 'words' });
+  userText = '';
+
+  userEnterText(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.userText = value;
+  }
+  compare(rendomLetter: string, userLetter: string) {
+    if (!userLetter) return 'panding';
+    return userLetter === rendomLetter ? 'correct' : 'incorrect';
+  }
 }
